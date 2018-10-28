c6524a40e9060dcd10eb142f407e79df49648fc0
Jacob Spade <jacobspade@Jacobs-MacBook-Pro.local>
2018-10-27 23:41:08 -0700

    initial commit

---
+++ src/components/SideMenu.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+class SideMenu extends Component {
+  render() {
+    return (
+      <div>
+        <ul className="list-group">
+          <li className="list-group-item bg-light rounded-0">Cras justo odio</li>
+          <li className="list-group-item bg-light">Dapibus ac facilisis in</li>
+          <li className="list-group-item bg-light">Morbi leo risus</li>
+          <li className="list-group-item bg-light">Porta ac consectetur ac</li>
+          <li className="list-group-item bg-light">Dapibus ac facilisis in</li>
+          <li className="list-group-item bg-light">Morbi leo risus</li>
+          <li className="list-group-item bg-light">Porta ac consectetur ac</li>
+          <li className="list-group-item bg-light">Dapibus ac facilisis in</li>
+          <li className="list-group-item bg-light">Morbi leo risus</li>
+          <li className="list-group-item bg-light">Porta ac consectetur ac</li>
+          <li className="list-group-item bg-light">Dapibus ac facilisis in</li>
+          <li className="list-group-item bg-light">Morbi leo risus</li>
+          <li className="list-group-item bg-light">Porta ac consectetur ac</li>
+          <li className="list-group-item bg-light">Dapibus ac facilisis in</li>
+          <li className="list-group-item bg-light">Morbi leo risus</li>
+          <li className="list-group-item bg-light">Porta ac consectetur ac</li>
+          <li className="list-group-item bg-light rounded-0">Vestibulum at eros</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default SideMenu;
